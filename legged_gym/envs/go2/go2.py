ef492d3649745a8d6f1257c368d4a9be590767b2
Shohei Nagai <terra1114@gmail.com>
2026-03-16 19:59:29 -0400

    remove go2.py

--- legged_gym/envs/go2/go2.py
+++
@@ -1,9 +0,0 @@
-from legged_gym.envs.base.legged_robot import LeggedRobot
-
-
-class Go2(LeggedRobot):
-
-    def check_termination(self):
-        super().check_termination()
-        # Terminate if the robot is flipped (projected gravity points upward)
-        self.reset_buf |= self.projected_gravity[:, 2] > 0
